5b1d20548b5ab15c5261dd13ac0bab5d53a950d5
marghherita <margherita.grasso3@gmail.com>
2022-01-21 23:26:35 +0100

    feat: fetch and logic

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import axios from 'axios';
+import { useEffect, useRef, useState } from 'react';
+
+function App() {
+
+
+  const [beers, setBeers] = useState('')
+  const [vol, setVol] = useState([])
+  const [des, setDes] = useState('')
+  const [beerImg, setBeerImg] = useState([])
+
+  const handleGenerator = () => {
+      
+      axios.get('https://api.punkapi.com/v2/beers/random')
+      .then((res) => {
+        console.log(res.data[0])
+        setBeers(res.data[0].name)
+        setVol(res.data[0].abv)
+        setBeerImg(res.data[0].image_url)
+        setDes(res.data[0].description)
+      })
+
+
+  }
+  
+
+  return (
+    <div className="App">
+      <h1>Random beer generator</h1>
+      <button onClick={handleGenerator}> GENERATE YOUR BEER </button>
+      <h2>{beers}</h2>
+      <h3>Alc. {vol}% by Vol</h3>
+      <div className='description'> 
+      <p>{des}</p>
+      </div>
+      <div>
+      <img className='beer-image' src={beerImg} alt={beers} />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
